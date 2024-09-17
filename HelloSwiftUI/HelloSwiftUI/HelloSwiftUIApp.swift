fccc7350b723e55124793dab12d106c3d675cb0b
drypot <drypot@gmail.com>
2024-09-17 22:12:28 +0900

    add HelloSwiftUI

---
+++ HelloSwiftUI/HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Kyuhyun Park on 9/17/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ HelloSwiftUI/HelloSwiftUI/Task/Counter.swift
@@ -0,0 +1,18 @@
+//
+//  Counter.swift
+//  HelloSwiftUI
+//
+//  Created by Kyuhyun Park on 6/6/24.
+//
+
+import Foundation
+
+struct Counter {
+    var counter = 0
+    
+    mutating func increment() -> Int {
+        let previous = counter
+        counter += 1
+        return previous
+    }
+}
