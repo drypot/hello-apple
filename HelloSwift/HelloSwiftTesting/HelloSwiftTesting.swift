fe49ca341a01ddb3ed64ab3e8db2047274db3d90
drypot <drypot@gmail.com>
2024-09-19 22:54:54 +0900

    add HelloSwiftTesting

---
+++ HelloSwift/HelloSwiftTesting/HelloSwiftTesting.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftTesting.swift
+//  HelloSwiftTesting
+//
+//  Created by Kyuhyun Park on 9/19/24.
+//
+
+import Testing
+
+struct HelloSwiftTesting {
+
+    @Test
+    func example() async throws {
+        #expect("abc" == "abc")
+    }
+
+}
